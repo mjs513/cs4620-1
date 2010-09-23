5bb9cf862046c40c70a1f9a7c704c5be1f579c2f
natorf@gmail.com <natorf@gmail.com@ebbd4279-5267-bd07-7df5-4dafc36418f6>
2010-09-23 22:11:44 +0000

    First commit.

---
+++ trunk/src/cs4620-1.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : cs4620-1.cpp
+// Author      : Renato Ramalho Fischer
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
